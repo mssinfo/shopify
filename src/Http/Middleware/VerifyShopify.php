c6874ab6f9f7d7ee84257c920e4ea762f9ce0261
mragank <mragankshekhar@gmail.com>
2023-06-24 12:00:27 +0530

    check shop in db

--- src/Http/Middleware/VerifyShopify.php
+++ src/Http/Middleware/VerifyShopify.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
+use Msdev2\Shopify\Models\Shop;
 
 class VerifyShopify
 {
@@ -16,8 +17,12 @@ class VerifyShopify
     public function handle(Request $request, Closure $next)
     {
         if (Str::contains($request->getRequestUri(), ['/auth/callback', '/install', '/billing']) || isset($request->shop)) {
+            $shop = Shop::where("shop")->first();
+            if(!$shop){
+                abort(403,'invalid shop domain');        
+            }
             return $next($request);
         }
-        abort(403);
+        abort(403,'shop not exist in request');
     }
 }
